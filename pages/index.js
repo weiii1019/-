111c672c983ba91c0eb4dabca01470d0a5f2f544
weiii1019 <11208115@gm.chihlee.edu.tw>
2026-04-27 09:50:22 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center", fontFamily: "Arial" }}>
+      <h1>⚡ 法說會供應鏈解碼器</h1>
+      <p>應用程式正在載入...</p>
+      <p style={{ color: "#666", fontSize: "12px" }}>
+        如果看到這個頁面，代表 Next.js 正常運作！
+      </p>
+    </div>
+  );
+}
